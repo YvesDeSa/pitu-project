5961351962ac0b3d7c5d33ed26d06f59fad10038
Yves de Sá <yvesdsb98@gmail.com>
2021-11-21 00:52:28 -0300

    Backend termination.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import LinksRouter from './routes/links';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(LinksRouter);
 
 export default app;
--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,5 +1,5 @@
 import {Sequelize} from 'sequelize';
 
-const sequelize = new Sequelize('mysql://yves:123456@localhost:3306/pitu');
+const sequelize = new Sequelize('mysql://root:mysql@localhost:3306/pitu');
 
 export default sequelize;
